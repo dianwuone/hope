6cd277ca8a763d7da60d92fb53d2d7a6b375c239
dianwu <dianwupei@163.com>
2026-05-12 18:15:10 +0800

    fix数据

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE = import.meta.env.DEV ? 'http://127.0.0.1:4100' : '/api'
+const DEFAULT_API_BASE = import.meta.env.DEV ? 'http://127.0.0.1:4100/api' : '/api'
 const API_BASE = (import.meta.env.VITE_API_BASE || DEFAULT_API_BASE).replace(/\/$/, '')
 const ENABLE_API_FALLBACK = import.meta.env.VITE_ENABLE_API_FALLBACK === 'true'
 
